234d45c779d485af712defda1489e3f744ff92c1
zzaynAli <bcsf19a022@pucit.edu.pk>
2022-10-06 14:21:31 +0500

    Done till PROPS

---
+++ module1.mjs
@@ -0,0 +1,8 @@
+const n1 = "Zain"
+const n2 = "Ali"
+const n3 = "Shan"
+
+export default n1
+
+export {n2}
+export {n3}
---
+++ module2.mjs
@@ -0,0 +1,4 @@
+import ui, {n2,n3} from './module1.mjs'
+console.log (ui)
+console.log(n2);
+console.log(n3)
