5c911037c553b0a5f34fdb951346ecd22b210cfa
zzaynAli <bcsf19a022@pucit.edu.pk>
2022-10-06 12:37:08 +0500

    Work Till Bootstrap Navbar

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // put every component in the div with id root
+root.render( //render that div having App Component in it
   <React.StrictMode>
     <App />
   </React.StrictMode>
