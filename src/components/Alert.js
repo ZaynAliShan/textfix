1ae74dab053b1ab7f65006a686f9cc191cdb83b5
Muhammad Zain Ali Shan <bcsf19a022@pucit.edu.pk>
2022-10-15 01:44:15 +0500

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,9 +8,14 @@ export default function Alert(props) {
   }
 
   return (
-    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-      <strong>{cap_first_letter(props.alert.type)}</strong> - {props.alert.msg}
-      {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+    <div style={{height: '42px'}}>
+      {
+        props.alert && 
+        <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+          <strong>{cap_first_letter(props.alert.type)}</strong> - {props.alert.msg}
+          {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+        </div>
+      }
     </div>
   )
   // we do pops.alert && --> to run from the null bug of alert (will find alert as null and rest of thig will not be given) so we see that if it's null don't evaluate other thing in above code
